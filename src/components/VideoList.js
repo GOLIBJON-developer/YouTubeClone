401f5de047a359cbe2815db31684f6e895550ee7
GOLIBJON <blackstaroffical88@gmail.com>
2022-02-12 01:55:57 +0500

    Fixed broken codes

---
+++ src/components/VideoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import VideoItem from "./VideoItem";
+const VideoList = ({ video, onVideoSelect }) => {
+  const listOfVideos = video.map((video, id) => (
+    <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />
+  ));
+  return (
+    <Grid container spacing={5}>
+      {listOfVideos}
+    </Grid>
+  );
+};
+
+export default VideoList;
